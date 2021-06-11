37bbed6bdabbb4b98571eb2c1471d319e792d0c9
nagam <nagam@DESKTOP-7SU2OP8>
2021-06-10 19:13:17 -0500

    Rama First commit

---
+++ src/test/java/Ramaclass1.java
@@ -0,0 +1,9 @@
+
+public class Ramaclass1 {
+	
+	public void meth1()
+	{
+		System.out.println("SriramaSitarama");
+	}
+
+}
---
+++ src/test/java/Ramaclass1Test2.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Ramaclass1Test2 {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void urlopen()
+	{
+		System.out.println("SriramaSitarama");
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\nagam\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		  
+		  driver=new ChromeDriver();
+		  driver.get("https://www.google.com/");
+	}
+	
+  @Test
+  public void setup() {
+	  
+	  WebElement gsearch= driver.findElement(By.name("q"));
+	  gsearch.sendKeys("java");
+	  gsearch.sendKeys(Keys.ENTER);
+	  
+	  
+  }
+    @AfterTest 
+    public void closeurl()
+    {
+    	
+    	driver.quit();
+    }
+  
+}
